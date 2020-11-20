9a131cda2730cca385cd16ace7bce52be2588854
madrid-7 <783798088@qq.com>
2020-11-21 00:21:02 +0800

    猫狗队列

---
+++ 猫狗队列/src/Pet.java
@@ -0,0 +1,23 @@
+public class Pet {
+    private String type;
+
+    public Pet (String type) {
+        this.type = type;
+    }
+
+    public String getPetType() {
+        return this.type;
+    }
+}
+
+class Dog extends Pet {
+    public Dog(String type) {
+        super("dog");
+    }
+}
+
+class Cat extends Pet {
+    public Cat(String type) {
+        super("cat");
+    }
+}
